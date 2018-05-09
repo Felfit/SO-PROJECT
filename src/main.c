376f00e863d25d7b8474626676f4d028b74abe3d
Dantas198 <xp1998@live.com.pt>
2018-05-09 18:53:33 +0100

    main c

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 /*
 #define MAX_SIZE 1024
 
@@ -11,44 +12,27 @@ typedef struct string
     char *line;
 }* String;
 
-
-String makeString (char* buf, int size){
-  String str = malloc(sizeof (struct string));
-  str->line = (char*) malloc(size+1);
-  str->line = buf+1;
-  str->size = size;
-  return str;
-}
-
 String readln(int fildes, int *n, int size){
 	int rd=1,i=0;
 	char c=' '; // ver se é preciso começar com ' '
-  char* buf = (char*)malloc(MAX_SIZE);
+  String str = malloc(sizeof( struct string));
+  str->line = (char*)malloc(MAX_SIZE);
 
     while(i<size && rd>0 && c!='\n'){
       rd = read(fildes, &c, 1); // lê 1 para já
-        //filtrar o que não são comandos
-        if(i == 0 && c!= '$') {
-          if(rd <= 0) *n = -1;
-          return NULL;
-        }
-        //passar para o buffer os caracteres ddo comando
-        else{
           if (rd && c!="\n"){
-            //if(i == 0) i++; // não funciona por algum motivo
-            *(buf + i) = c;}
-          }
+            str->line[i] = c;}
         i++;
     }
 
     if(i == 0) return NULL; //ver este caso
     if(rd<=0) *n = -1;
     else{
-      *(buf + i) = '\0'; // não é preciso, visto que temos size, mas deixo para já.
+      str->line[i] = '\0';
       *n = i;
     }
-  String result = makeString(buf, i);
-  return result;
+  str->size = i;
+  return str;
 }
 
 void readfromFile( char *filepath){ //notebook a
@@ -83,7 +67,7 @@ int main(int argc,char *argv[])
 		fprintf(stderr,"Use ./program <dumb_path>\n");
 		return 0;
 	}
-    //readfromFile(argv[1]);
+    readfromFile(argv[1]);
 
 return 0;
 }
