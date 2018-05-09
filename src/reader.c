dcf0979465b9e342d4b4e9f801a9f123834a9be3
Dantas198 <xp1998@live.com.pt>
2018-05-09 02:47:37 +0100

    reader quase pronto, mas não insere na estrutura

--- src/reader.c
+++ src/reader.c
@@ -2,43 +2,63 @@
 #include "reader.h"
 #define MAX_SIZE 1024
 
+
+String makeString (char* buf, int size){
+  String str = malloc(sizeof (struct string));
+  str->line = (char*) malloc(size+1);
+  str->line = buf;
+  str->size = size;
+  return str;
+}
+
 String readln(int fildes, int *n, int size){
 	int rd=1,i=0;
-	char c; // ver se é preciso começar com ' '
-  String result = malloc(sizeof(struct string));
+	char c=' '; // ver se é preciso começar com ' '
+  char* buf = (char*)malloc(MAX_SIZE);
 
     while(i<size && rd>0 && c!='\n'){
       rd = read(fildes, &c, 1); // lê 1 para já
-        if (rd && c!='\n')
-          result->line[i] = c;
+        //filtrar o que não são comandos
+        if(i == 0 && c!= '$') {
+          if(rd <= 0) *n = -1;
+          return NULL;
+        }
+        //passar para o buffer os caracteres ddo comando
+        else{
+          if (rd && c!="\n"){
+            //if(i == 0) i++; // não funciona por algum motivo
+            *(buf + i) = c;}
+          }
         i++;
     }
 
     if(i == 0) return NULL; //ver este caso
-    if(rd<0) *n = rd;
+    if(rd<=0) *n = -1;
     else{
-      result->line[i] = '\0'; // não é preciso, visto que temos size, mas deixo para já.
+      *(buf + i) = '\0'; // não é preciso, visto que temos size, mas deixo para já.
       *n = i;
     }
-  result->size = i;
+  String result = makeString(buf, i);
   return result;
 }
 
-/**
- * Le coisas de um array
- * Passa linhas na estrutura String ao Notebook
- *
-*/
 void readfromFile(Notebook a, char *filepath){ //notebook a
   int fd, n=0;
     if((fd = open(filepath, O_RDONLY, 0644)) > 0){
       while(n >= 0){
-        //falta meter para só pegar no que começa por $ !!!!!
-        //posso fazer um read aqui para não ter de mandar NULL ou então retorno NULL na readln
         String tmp = readln(fd, &n, MAX_SIZE);
-        //enviar para notebook
-          printf("%s\n",tmp->line);
+            if(tmp){
+              //mete no notebook
+              /*
+              int j=0;
+              while(tmp->line[j]!='\0'){
+                printf("%c",tmp->line[j]);
+                j++;
+              }
+            }*/
+
     }
   }
     else perror("Can't open this file!");
+  close(fd);
 }
